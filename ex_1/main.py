ef136ed4eb7abcf0f1b617cd309d90b2d6b46057
kevingeng <kevingenghaopeng@gmail.com>
2021-04-21 17:29:41 +0900

    add test file

--- ex_1/main.py
+++ ex_1/main.py
@@ -8,7 +8,7 @@ import pdb
 
 
 def calc_fft(data, samplerate):
-    spectrum = fftpack.fft(data)
+    spectrum = scipy.fftpack.fft(data)
     amp = np.sqrt((spectrum.real ** 2) + (spectrum.imag ** 2))
     amp = amp / (len(data) / 2)
     phase = np.arctan2(spectrum.imag, spectrum.real)
@@ -16,7 +16,7 @@ def calc_fft(data, samplerate):
     freq = np.linspace(0, samplerate, len(data))
     return spectrum, amp, phase, freq
 
-def slice_into_windows(data, win_len):
+def stft(data, win_len):
     start = 0
     window_n = np.hanning(win_len)
     start_points = [int(start)]
@@ -31,24 +31,24 @@ def slice_into_windows(data, win_len):
             slices.append(np.pad(valid_signal, (0, win_len-len(valid_signal)), 'constant'))
         start += win_len // 2
     slices = np.array(slices)
-    
     # windowing
     windowed_slices = [window_n * slices[i] for i in range(len(slices[0:]))]
-    pdb.set_trace()
 
     spec = []
     for short_slice in slices:
         fft_result = np.fft.fft(short_slice)
-        spec.append(np.abs(fft_result))
+        spec.append(np.abs(fft_result))    
+    freqs = np.fft.fftfreq(len(fft_result)) 
     spec = np.array(spec)
-    # return slices
+    pdb.set_trace()
+    return spec
 
 if __name__ == "__main__":
     input_wav = '/home/kevingeng/nas01/home/JNAS/WAVES_DT/F002/NP/NF002001_DT.wav'
     y, sr = librosa.load(input_wav)
     win_len = 1024
-    slice_into_windows(y, win_len=1024)
-
+    spec = stft(y, win_len=win_len)
+    pdb.set_trace()
     # amp = np.abs(a)
     # N = len(amp)
     # amp_normal = amp / (N / 2)
