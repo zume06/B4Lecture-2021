0758ba9fe50ecca35532995430e9fcb05c3ec930
Tomohiro <tomohiro@Tomohiro-Macbook-Pro.local>
2021-04-25 21:06:12 +0900

    add comment

--- ex_1/t_suzuki/ex_1.py
+++ ex_1/t_suzuki/ex_1.py
@@ -8,14 +8,15 @@ out_dir = "./out"
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
+# 音声ファイルの読み込み
 fname = "./sound/source.aifc"
 y, sr = librosa.load(fname)
 total_time = np.arange(0, len(y)) / sr
+# 窓幅・窓関数の設定
 window = 1024
-overlap = window / 2
-step = 512
 win_func = signal.hamming(window)
 
+# フーリエ変換
 spec = []
 fft_data = []
 for idx in range(0, len(y), 512):
@@ -23,10 +24,12 @@ for idx in range(0, len(y), 512):
     if len(frame) == 1024:
         frame = frame * win_func
         fft_res = np.fft.rfft(frame)
+        # 逆変換用のデータを準備
         fft_data.append(fft_res)
         res = np.log(np.abs(fft_res) ** 2)
         spec.append(res)
 
+# 逆変換
 ifft_res = []
 for i in range(len(fft_data)):
     f = np.fft.irfft(fft_data[i])
@@ -34,9 +37,11 @@ for i in range(len(fft_data)):
         ifft_res.extend(f)
     else:
         ifft_res.extend(f[512:1024])
+# 欠損値をゼロ埋め
 for i in range(len(total_time) - len(ifft_res)):
     ifft_res.append(0)
 
+# オリジナルの信号と逆変換後の信号を描画・保存
 fig1 = plt.figure(tight_layout=True)
 plt.subplot(2, 1, 1)
 plt.plot(total_time, y)
@@ -51,6 +56,7 @@ plt.ylabel("Magnitude")
 plt.show(block=True)
 fig1.savefig(f"{out_dir}/Signal.png")
 
+# スペクトログラムを描画・保存
 fig2 = plt.figure()
 plt.imshow(np.array(spec).T, extent=[0, total_time[-1], 0, (sr/2) / 1000], aspect="auto")
 plt.title("Spectrogram")
