6a24df8bbd35ff2b88331c9a067eb9b82f353fcf
梶原大進 <daishin@kajiwarataishinnoMacBook-puro.local>
2021-04-25 15:26:21 +0900

    calculation time measurement

---
+++ ex_1/kajiwara/bench.py
@@ -0,0 +1,42 @@
+import time
+
+import scipy as sp
+import numpy as np
+import librosa
+
+from main import stft, instft
+
+
+def calc_process_time(args, func):
+    start = time.perf_counter()
+    func(*args)
+    end = time.perf_counter()
+
+    return end - start
+
+
+def my_spectrogram(data):
+    # stft
+    cs = stft(data, win_size=1024, overlap=0.5)
+
+    # extract magnitude (amplitude spectrum) and phase (phase spectrum)
+    # amplitude, phase = librosa.magphase(cs)
+    # amplitude -> db
+    db = librosa.amplitude_to_db(np.abs(cs))
+
+    return db
+
+
+def main():
+    fake_data = 2 * np.random.rand(22050*3) - 1
+
+    # scipy
+    sp_res = calc_process_time([fake_data], sp.signal.spectrogram)
+    print("scipy.signal.spectrogram result: {}".format(sp_res))
+
+    # mine
+    my_res = calc_process_time([fake_data], my_spectrogram)
+    print("my spectrogram result: {}".format(my_res))
+
+
+main()
