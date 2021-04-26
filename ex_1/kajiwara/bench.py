401b88f7e4cb9e2ef50cfeb9d525bda1a2bbd04c
梶原大進 <daishin@kajiwarataishinnoMacBook-puro.local>
2021-04-26 20:59:25 +0900

    mics

--- ex_1/kajiwara/bench.py
+++ ex_1/kajiwara/bench.py
@@ -15,6 +15,14 @@ def calc_process_time(args, func):
     return end - start
 
 
+def librosa_spectrogram(data):
+    cv = librosa.stft(data)
+    # s, phase = librosa.magphase(cv)
+    db = librosa.amplitude_to_db(np.abs(cv))
+
+    return db
+
+
 def my_spectrogram(data):
     # stft
     cs = stft(data, win_size=1024, overlap=0.5)
@@ -30,14 +38,31 @@ def my_spectrogram(data):
 def main():
     fake_data = 2 * np.random.rand(22050*3) - 1
 
-    # scipy
-    sp_res = calc_process_time(
-        {'x': fake_data, 'mode': 'complex'}, sp.signal.spectrogram)
-    print("scipy.signal.spectrogram result: {}".format(sp_res))
+    res = {
+        'scipy': [],
+        'librosa': [],
+        'mine': []
+    }
 
-    # mine
-    my_res = calc_process_time({'data': fake_data}, my_spectrogram)
-    print("my spectrogram result: {}".format(my_res))
+    for i in range(10):
+        # scipy
+        sp_res = calc_process_time(
+            {'x': fake_data, 'mode': 'complex'}, sp.signal.spectrogram)
+        res['scipy'].append(sp_res)
+        print("scipy.signal.spectrogram result: {}".format(sp_res))
+
+        # librosa
+        lib_res = calc_process_time({'data': fake_data}, librosa_spectrogram)
+        res['librosa'].append(lib_res)
+        print("using librosa dtft result: {}".format(lib_res))
+
+        # mine
+        my_res = calc_process_time({'data': fake_data}, my_spectrogram)
+        res['mine'].append(my_res)
+        # print("my spectrogram result: {}".format(my_res))
+
+    for k, v in res.items():
+        print(k, sum(v)/len(v))
 
 
 main()
