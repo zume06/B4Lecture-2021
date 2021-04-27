b1a30632a97b6480c54a29ef7ef43a42b679da9d
梶原大進 <daishin@kajiwarataishinnoMacBook-puro.local>
2021-04-27 13:58:13 +0900

    add comment and fix bench

--- ex_1/kajiwara/bench.py
+++ ex_1/kajiwara/bench.py
@@ -3,8 +3,12 @@ import time
 import scipy as sp
 import numpy as np
 import librosa
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-from main import stft, instft
+from main import get_spectrogram
+
+TIME_TEMPLATE = '%Y%m%d%H%M%S'
 
 
 def calc_process_time(args, func):
@@ -23,46 +27,75 @@ def librosa_spectrogram(data):
     return db
 
 
-def my_spectrogram(data):
-    # stft
-    cs = stft(data, win_size=1024, overlap=0.5)
-
-    # extract magnitude (amplitude spectrum) and phase (phase spectrum)
-    # amplitude, phase = librosa.magphase(cs)
-    # amplitude -> db
-    db = librosa.amplitude_to_db(np.abs(cs))
-
-    return db
-
-
 def main():
-    fake_data = 2 * np.random.rand(22050*3) - 1
+    length = [
+        22050*1,
+        22050*10,
+        22050*50,
+        22050*100,
+        22050*150,
+        22050*500,
+        22050*1000,
+        22050*1500,
+    ]
+    result = []
 
-    res = {
+    for l in length:
+        print(l)
+        fake_data = 2 * np.random.rand(l) - 1
+
+        res = {
+            'scipy': [],
+            'librosa': [],
+            'mine_normal': [],
+            'mine_faster': [],
+        }
+
+        for i in range(10):
+            # scipy
+            sp_res = calc_process_time(
+                {'x': fake_data, 'mode': 'complex'}, sp.signal.spectrogram)
+            res['scipy'].append(sp_res)
+
+            # librosa
+            lib_res = calc_process_time(
+                {'data': fake_data}, librosa_spectrogram)
+            res['librosa'].append(lib_res)
+
+            # mine(normal)
+            my_res = calc_process_time(
+                {'wave_data': fake_data, 'mode': 'normal'}, get_spectrogram)
+            res['mine_normal'].append(my_res)
+
+            # mine(faster)
+            my_res = calc_process_time(
+                {'wave_data': fake_data, 'mode': 'faster'}, get_spectrogram)
+            res['mine_faster'].append(my_res)
+
+        result.append(res)
+
+    aves = {
         'scipy': [],
         'librosa': [],
-        'mine': [],
+        'mine_normal': [],
+        'mine_faster': [],
     }
+    for i, res in enumerate(result):
+        print('-'*10)
+        for k, v in res.items():
+            print(k, sum(v)/len(v))
+            aves[k].append(sum(v)/len(v))
+    print('-'*10)
 
-    for i in range(10):
-        # scipy
-        sp_res = calc_process_time(
-            {'x': fake_data, 'mode': 'complex'}, sp.signal.spectrogram)
-        res['scipy'].append(sp_res)
-        print("scipy.signal.spectrogram result: {}".format(sp_res))
-
-        # librosa
-        lib_res = calc_process_time({'data': fake_data}, librosa_spectrogram)
-        res['librosa'].append(lib_res)
-        print("using librosa dtft result: {}".format(lib_res))
-
-        # mine
-        my_res = calc_process_time({'data': fake_data}, my_spectrogram)
-        res['mine'].append(my_res)
-        # print("my spectrogram result: {}".format(my_res))
-
-    for k, v in res.items():
-        print(k, sum(v)/len(v))
+    p_scipy = plt.plot(length, aves['scipy'], marker="o", label='scipy')
+    p_librosa = plt.plot(length, aves['librosa'], marker="o", label='librosa')
+    p_normal = plt.plot(
+        length, aves['mine_normal'], marker="o", label='normal')
+    p_faster = plt.plot(
+        length, aves['mine_faster'], marker="o", label='faster')
+    plt.legend(loc=2)
+    timestamp = datetime.now().strftime(TIME_TEMPLATE)
+    plt.savefig('bench-res-{}.png'.format(timestamp))
 
 
 main()
