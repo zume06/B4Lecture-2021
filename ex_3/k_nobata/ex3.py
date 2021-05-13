2cc39d7a67e9f4d04bb54664bcac2cebcabe7f29
k_nobata <nobata.koki@g.sp.m.is.nagoya-u.ac.jp>
2021-05-13 21:32:17 +0900

    3d 回帰式

--- ex_3/k_nobata/ex3.py
+++ ex_3/k_nobata/ex3.py
@@ -4,16 +4,38 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 #2次元散布図
-def pdscatter(df,x,y):
+def pdscatter2(df, x, y):
+    """
+    paramerters
+    --
+    df:pandas.core.frame.DataFrame
+       dataframe of csv file
+    x:numpy.ndarray
+      explanatory variable
+    y:numpy.ndarray
+      explained variable
+    """
     plt.scatter(df["x1"], df["x2"])
-    plt.plot(x,y,color="green")
+    plt.plot(x,y,color="pink")
     plt.xlabel("x1")
     plt.ylabel("x2")
     plt.grid()
     plt.show()
 
 #3次元散布図
-def pdscatter3(df):
+def pdscatter3(df, x, y, z):
+    """
+    paramerters
+    --
+    df:pandas.core.frame.DataFrame
+       dataframe of csv file
+    x:numpy.ndarray
+      explanatory variable
+    y:numpy.ndarray
+      explanatory variable
+    z:numpy.ndarray
+      explained variable
+    """
     fig = plt.figure()
     ax = Axes3D(fig)
     
@@ -22,12 +44,13 @@ def pdscatter3(df):
     ax.set_zlabel("x3")
 
     ax.plot(df["x1"], df["x2"], df["x3"] ,marker="o",linestyle='None')
+    ax.plot_wireframe(x, y, z,color="pink")
 
     plt.grid()
     plt.show()
 
-#理論値作る
-def ideal(df,K,w):
+#2次元の理論値作る
+def ideal2(df, K):
     """
     paramerters
     --
@@ -35,32 +58,79 @@ def ideal(df,K,w):
        dataframe of csv file
     K:int
       degree of regression equation
-    w:numpy.ndarray
-    
+      
     returns
-    ide_1:np.ndarray
-          x
-    ide_2:np.ndarray
-          y
+    --
+    ide_x:np.ndarray
+          explanatory variable
+    ide_y:np.ndarray
+          explained variable
     """
-    ide_1 = np.arange(min(df["x1"]), max(df["x1"]), 0.01)
-    ide_2 = np.zeros(len(ide_1))
-    for i in range(len(ide_1)):       
+    w = regression2(df, K)
+    ide_x = np.arange(min(df["x1"]), max(df["x1"]), 0.01)
+    ide_y = np.zeros(len(ide_x))
+    
+    for i in range(len(ide_x)):       
         for j in range(K+1):
-            ide_2[i] += w[j] * (ide_1[i] ** j)
-    return ide_1, ide_2
+            ide_y[i] += w[j] * (ide_x[i] ** j)
+            
+    return ide_x, ide_y
 
-def regression(ar_x,ar_y,K):
+#3次元の理論値作る
+def ideal3(df, K1, K2):
     """
     paramerters
+    --
+    df:pandas.core.frame.DataFrame
+       dataframe of csv file
+    K1 and K2:int
+              degree of regression equation
+      
+    returns
+    --
+    ide_z:np.ndarray
+          explanatory variable
+    ide_y:np.ndarray
+          explanatory variable
+    ide_z:np.ndarray
+          explained variable
+    """
+    
+    w = regression3(df, K1, K2)
+    ide_x = np.arange(min(df["x1"]), max(df["x1"]), 0.01)    
+    ide_y = np.linspace(min(df["x2"]), max(df["x2"]), len(ide_x))
+    ide_z = np.zeros([len(ide_y),len(ide_x)])
+    
+    mesh_x, mesh_y = np.meshgrid(ide_x, ide_y)
+    
+    for i in range(len(ide_y)):
+        for m in range(len(ide_x)):
+            for j in range(K1+K2+1):
+                if j <= K1:
+                    ide_z[i,m] += (w[j] * (ide_x[m] ** j))
+                else:
+                    ide_z[i,m] += (w[j] * (ide_y[i] ** (j-K1)))
+                    
+    return mesh_x, mesh_y, ide_z
+
+#2次元の回帰係数
+def regression2(df,K):
+    """
+    paramerters
+    --
     df:pandas.core.frame.DataFrame
        dataframe of csv file
     K:int
       degree of regression equatio
       
     returns
-    w:
+    --
+    w:numpy.ndarray
+      regression coefficient
     """
+    ar_x = np.array(df["x1"])
+    ar_y = np.array(df["x2"])
+    
     #基底関数行列Φ
     Len_x = len(ar_x)
     phi = np.zeros([Len_x, K+1])
@@ -70,33 +140,58 @@ def regression(ar_x,ar_y,K):
             phi[i, k] = ar_x[i] ** k
     
     #最小二乗法
-    w = np.linalg.inv(phi.T@phi)@phi.T@ar_y   #linalg:逆行列　@:内積
+    w = np.linalg.inv(phi.T@phi)@phi.T@ar_y   #linalg.inv:逆行列　@:内積
+    
+    return w
+
+#3次元の回帰係数
+def regression3(df, K1, K2):
+    """
+    paramerters
+    --
+    df:pandas.core.frame.DataFrame
+       dataframe of csv file
+    K1 and K2:int
+              degree of regression equatio
+      
+    returns
+    --
+    w:numpy.ndarray
+      regression coefficient
+    """
+    ar_x = np.array(df["x1"])
+    ar_y = np.array(df["x2"])
+    ar_z = np.array(df["x3"])
+    
+    #基底関数行列Φ
+    Len_x = len(ar_x)
+    phi = np.zeros([Len_x, K1 + K2 +1]) #定数項は一つ
+    
+    for i in range(Len_x):
+        for j in range(K1+1):
+            phi[i, j] = ar_x[i] ** j
+    for l in range(Len_x):
+        for m in range(K2):
+            phi[l, K1 + m + 1] = ar_y[l] ** (m+1)
+    
+    
+    #最小二乗法
+    w = np.linalg.inv(phi.T@phi)@phi.T@ar_z   #linalg.inv:逆行列　@:内積
     
     return w
 
 def main():
-
     df1 = pd.read_csv("/Users/nobatakoki/B4輪行/B4Lecture-2021/ex_3/data1.csv")
     df2 = pd.read_csv("/Users/nobatakoki/B4輪行/B4Lecture-2021/ex_3/data2.csv")
     df3 = pd.read_csv("/Users/nobatakoki/B4輪行/B4Lecture-2021/ex_3/data3.csv")
-    
-    ar1_x = np.array(df1["x1"])
-    ar1_y = np.array(df1["x2"])
-    ar2_x = np.array(df2["x1"])
-    ar2_y = np.array(df2["x2"])
-    ar3_x = np.array(df3["x1"])
-    ar3_y = np.array(df3["x2"])
-    ar3_z = np.array(df3["x3"])
 
-    w1 = regression(ar1_x,ar1_y,1)
-    x1, y1 = ideal(df1,1,w1)
-    w2 = regression(ar2_x,ar2_y,3)
-    x2, y2 = ideal(df2, 3, w2)
+    x1, y1 = ideal2(df1, 1)
+    x2, y2 = ideal2(df2, 3)
+    x3, y3, z3 = ideal3(df3,3,3)
 
-    pdscatter(df1, x1, y1)
-    pdscatter(df2, x2, y2)
-    pdscatter3(df3)
+    pdscatter2(df1, x1, y1)
+    pdscatter2(df2, x2, y2)
+    pdscatter3(df3, x3, y3, z3)
 
-
-if __name__ == "__main__":
-    main()
+if __name__ =="__main__":
+      main()
