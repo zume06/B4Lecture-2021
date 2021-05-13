06bac1b8e0c4511bd6cdbe0514d33ea96bfe7baa
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2021-05-13 18:20:48 +0900

    add generate_degs etc

--- ex_3/a_miyashita/main.py
+++ ex_3/a_miyashita/main.py
@@ -1,26 +1,48 @@
 import argparse
 
 import numpy as np
+import matplotlib as mpl
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-def generalized_inv(x):
-    return np.linalg.inv(x.T @ x) @ x.T
 
 class LinearModel:
-
     def __init__(self, base_functions):
+        """
+        Class for linear regression.
+
+        # Parameters
+            base_function (list):
+                List of base function. Each of functions must take ndarray as an argument,
+                culculate results elementwise along its 1st dimension, and return 1d-ndarray.
+        """
         self.base_functions = base_functions
         self.w = np.zeros(len(base_functions))
 
-    def fit(self, x, y):
+    def fit(self, x, y, lamda=0.0):
+        """
+        Fit weights to given data.
+
+        # parameters
+            x   (ndarray): independent variavle
+            y   (ndarray): dependent variable
+            lamda (float): regularize coefficient
+        """
         phi = np.zeros((x.shape[0], self.w.shape[0]))
 
         for i in range(phi.shape[1]):
             phi[:, i] = self.base_functions[i](x)
 
-        self.w =  generalized_inv(phi) @ y
+        # solve normal equation
+        self.w = np.linalg.inv(phi.T @ phi + lamda * np.eye(phi.shape[1])) @ phi.T @ y
 
     def predict(self, x):
+        """
+        Predict dependent variable corresponds to inputs by using learnt weights.
+
+        # parameters
+            x (ndarray): indepedent variable
+        """
         phi = np.zeros((x.shape[0], self.w.shape[0]))
 
         for i in range(phi.shape[1]):
@@ -28,78 +50,170 @@ class LinearModel:
 
         return phi @ self.w
 
+    def name(self):
+        name = "$y="
+        name += "{:.2f}".format(self.w[0]) + self.base_functions[0].name()
+        for i in range(1, self.w.size):
+            name += "{:+.2f}".format(self.w[i]) + self.base_functions[i].name()
+        name += "$"
+        return name
+
 
 class polynomial:
+    def __init__(self, degs):
+        """
+        Class of polynomial function.
 
-    def __init__(self, dim_list):
-        self.dim_list = dim_list
+        For example, polynomial([1, 0, 2])(x) can be used as
+        (x[0] ** 1) * (x[1] ** 0) * (x[2] ** 2).
+
+        # Parameter
+            degs (ndarray, shape=(n,)):
+                degree-array. n is number of variable.
+        """
+        self.degs = degs
 
     def __call__(self, x):
+        # culculate power variable by variable
+        pow = x ** self.degs
+
         if x.ndim == 1:
-            return x ** self.dim_list[0]
-
+            return pow
         else:
-            result = 1.0
-            for i in range(len(self.dim_list)):
-                result *= x[:, i] ** self.dim_list[i]
+            # clculate product
+            return np.prod(pow, axis=1)
 
-            return result
+    def name(self):
+        name = ""
+        for i in range(self.degs.size):
+            if self.degs[i] > 0:
+                name += "x_{{{}}}".format(i)
+                if self.degs[i] > 1:
+                    name = "{" + name + "}^" + "{{{:.0f}}}".format(self.degs[i])
+        return name
+
+
+def generate_degs(deg_sum, variable_size):
+    """
+    Generate degree-array for class polynomial.
+
+    # Parameters
+        deg_sum         (int):
+            the upper of sum of degree
+        variable_size   (int):
+            the number of variables used in polynomial
+
+    # Return
+        out: 1d-list (deg_sum + 1,) of 2d-ndarray (n, variable_size).
+
+        i-th ndarray consists of every degree-array such that
+        sum of degree is equal to i.
+
+    # Example
+        >>> degs = generate_degs(2, 3)
+        >>> print(degs)
+        [array([[0, 0, 0]]),
+         array([[1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1]]),
+         array([[2, 0, 0],
+                [1, 1, 0],
+                [1, 0, 1],
+                [0, 2, 0],
+                [0, 1, 1],
+                [0, 0, 2]])]
+    """
+
+    if variable_size == 1:
+        result = []
+        for i in range(0, deg_sum + 1):
+            degs_i = np.array(i).reshape(1, 1)
+            result.append(degs_i)
+
+        return result
+    else:
+        # culculate on 1 less variables
+        degs = generate_degs(deg_sum, variable_size - 1)
+
+        result = []
+        for i in range(0, deg_sum + 1):
+            # degree-array that sum of degree is equal to i
+            degs_i = np.array([]).reshape(0, variable_size)
+            for j in range(0, i + 1):
+                # insert shortage of degree
+                shortage = np.full((degs[j].shape[0], 1), i - j)
+                degs_i_j = np.insert(degs[j], 0, shortage, axis=1)
+
+                degs_i = np.append(degs_i, degs_i_j, axis=0)
+
+            result.append(degs_i)
+
+        return result
 
 
 if __name__ == "__main__":
     # process args
     parser = argparse.ArgumentParser(description="Linear model fitting")
-    parser.add_argument("fname", type=str, help="input filename with extension .csv")
     parser.add_argument(
-        "dim", type=int, help="dimension of polynomial used as base functions"
+        "fname", type=str, help="input filename followed by extension .csv"
     )
-    parser.add_argument("lambda", type=float, help="regularizer coefficient")
+    parser.add_argument(
+        "deg", type=int, help="degree of polynomial used as base function"
+    )
+    parser.add_argument("lamda", type=float, help="regularizer coefficient")
     args = parser.parse_args()
 
-    data = np.loadtxt(args.fname, delimiter=',', skiprows=1)
+    data = np.loadtxt(args.fname + ".csv", delimiter=",", skiprows=1)
 
-    x = data[: ,: -1]
+    # split to dependent and independent
+    x = data[:, :-1]
     y = data[:, -1]
-    print(x.shape)
-    print(y.shape)
 
+    # generate polynomial base function
+    degs = generate_degs(args.deg, x.shape[1])
     base_functions = []
-    for i in range((args.dim + 1) ** x.shape[1]):
-        j = i
-        dim_list = [0] * x.shape[1]
-        for k in range(x.shape[1]):
-            dim_list[k] = j % (args.dim + 1)
-            j = j // (args.dim + 1)
-        base_functions.append(polynomial(dim_list))
+    for d in degs:
+        for i in range(d.shape[0]):
+            base_functions.append(polynomial(d[i]))
 
     model = LinearModel(base_functions)
 
-    model.fit(x, y)
+    # model fitting
+    model.fit(x, y, lamda=args.lamda)
 
-    x1 = np.linspace(-6, 6)
-    prediction = model.predict(x1)
-    plt.plot(x1, prediction)
-    plt.scatter(x[:, 0], y)
-    plt.title("data1")
-    plt.show()
+    if x.shape[1] == 1:
+        x1 = np.linspace(x.min(), x.max())
 
-    """
-    x1 = np.linspace(-5, 5)
-    x2 = np.linspace(0, 10)
+        # predict
+        prediction = model.predict(x1)
 
-    X1, X2 = np.meshgrid(x1, x2)
-    X = np.concatenate([X1.reshape(-1, 1), X2.reshape(-1, 1)], axis=1)
+        # plot
+        plt.plot(x1, prediction, label=model.name())
+        plt.scatter(x[:, 0], y, c="r", label="Observed data")
+        plt.title(args.fname)
+        plt.xlabel("$x_0$")
+        plt.ylabel("$y$")
+        plt.legend(bbox_to_anchor=(1, 0), loc="lower right", borderaxespad=1)
+        plt.show()
 
-    prediction = model.predict(X)
-    prediction = prediction.reshape(50, 50)
+    if x.shape[1] == 2:
+        x1 = np.linspace(x[:, 0].min(), x[:, 0].max())
+        x2 = np.linspace(x[:, 1].min(), x[:, 1].max())
+        X1, X2 = np.meshgrid(x1, x2)
+        X = np.concatenate([X1.reshape(-1, 1), X2.reshape(-1, 1)], axis=1)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    surf = ax.plot_surface(X1, X2, prediction, cmap='rainbow', linewidth=0)
-    fig.colorbar(surf)
-    ax.set_title("data3")
+        # predict
+        prediction = model.predict(X)
+        prediction = prediction.reshape(50, 50)
 
-    ax.scatter3D(x[:, 0], x[:, 1], y)
-    ax.view_init(elev=10, azim=10)
-    plt.show()
-    """
+        # plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
+        ax.plot_wireframe(X1, X2, prediction, label=model.name(), linewidth=0.2)
+        ax.scatter3D(x[:, 0], x[:, 1], y, c="r", label="Observed data")
+        ax.set_title(args.fname)
+        ax.set_xlabel("$x_0$")
+        ax.set_ylabel("$x_1$")
+        ax.set_zlabel("$y$")
+        ax.legend()
+        plt.show()
