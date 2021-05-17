ce0393e7c1a80448b7656f29d22f0ca5eeabd1ae
梶原大進 <daishin@kajiwarataishinnoMacBook-puro.local>
2021-05-17 10:40:48 +0900

    regression equation

--- ex_3/kajiwara/linear_regression.py
+++ ex_3/kajiwara/linear_regression.py
@@ -1,3 +1,6 @@
+import itertools
+import collections
+
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 
@@ -5,10 +8,13 @@ from sklearn.preprocessing import PolynomialFeatures
 class LinearRegression:
     def __init__(self, digree=1):
         self.digree = digree
+        self.v_num = 0
+        self.coef = None
 
     def fit(self, X, y, sample_weight=None):
         '''
         Fit linear model.
+
         Parameters
         ----------
         X : array
@@ -17,10 +23,13 @@ class LinearRegression:
             Target values
         sample_weight : array, default=None
             Individual weights for each sample
+
         Returns
         -------
         self : returns an instance of self.
         '''
+
+        self.v_num = X.shape[1]
 
         poly = PolynomialFeatures(self.digree)
         phi = poly.fit_transform(X)
@@ -35,3 +44,41 @@ class LinearRegression:
         self.coef = np.dot(np.linalg.pinv(A), B)
 
         return self
+
+    def get_equation(self):
+        '''
+        return regression equation as string
+
+        Returns
+        -------
+        reg_eq : string
+            regression equation
+        '''
+
+        assert self.coef is not None, 'please fit first'
+
+        reg_eq = 'y='
+        v_list = ['x'+str(i) for i in range(self.v_num)]
+        comb_list = []
+        for dig in range(self.digree + 1):
+            for comb in itertools.combinations_with_replacement(v_list, dig):
+                c = collections.Counter(comb)
+                c_str = ''
+                for k, v in c.items():
+                    if v == 0:
+                        continue
+                    elif v == 1:
+                        c_str += k
+                    else:
+                        c_str += k + '**' + str(v)
+                comb_list.append(c_str)
+
+        assert len(comb_list) == len(self.coef), 'failed'
+
+        for i, c in enumerate(self.coef):
+            if i != 0:
+                reg_eq += '+'
+
+            reg_eq += '{:.2f}{}'.format(c, comb_list[i])
+
+        return reg_eq
