21601e2ae322d215c1b4948ee9b0dbaa391feb91
梶原大進 <daishin@kajiwarataishinnoMacBook-puro.local>
2021-05-17 10:40:48 +0900

    regression equation

--- ex_3/kajiwara/main.py
+++ ex_3/kajiwara/main.py
@@ -27,30 +27,34 @@ def main(args):
     X = df1['x1'].values.reshape(-1, 1)
     y = df1['x2'].values
     model_1.fit(X, y)
+    reg_eq_1 = model_1.get_equation()
 
     w = model_1.coef
     linex = np.linspace(-5, 5)
     y_ = w[1]*linex + w[0]
 
     plt.figure(figsize=(5, 5))
-    plt.scatter(X, y)
-    plt.plot(linex, y_, c='indianred')
-    plt.show()
+    plt.scatter(X, y, label='obserbed')
+    plt.plot(linex, y_, c='indianred', label=reg_eq_1)
+    plt.legend()
+    plt.savefig(result_path/'data1.png')
 
     # data2
     model_2 = LinearRegression(digree=3)
     X = df2['x1'].values.reshape(-1, 1)
     y = df2['x2'].values
     model_2.fit(X, y)
+    reg_eq_2 = model_2.get_equation()
 
     w = model_2.coef
     linex = np.linspace(0, 10)
     y_ = w[3]*linex**3 + w[2]*linex**2 + w[1]*linex + w[0]
 
     plt.figure(figsize=(5, 5))
-    plt.scatter(X, y)
-    plt.plot(linex, y_, c='indianred')
-    plt.show()
+    plt.scatter(X, y, label='obserbed')
+    plt.plot(linex, y_, c='indianred', label=reg_eq_2)
+    plt.legend()
+    plt.savefig(result_path/'data2.png')
 
     # data3
     model_3 = LinearRegression(digree=2)
@@ -59,9 +63,9 @@ def main(args):
     y = df3['x3'].values
     X = np.array([X1, X2]).T
     model_3.fit(X, y)
+    reg_eq_3 = model_3.get_equation()
 
     w = model_3.coef
-    # y_ = w[0]*X[:,0]+w[1]*X[:,1]+w[2]
     linex1 = np.linspace(-4, 4)
     linex2 = np.linspace(0, 10)
 
@@ -71,8 +75,9 @@ def main(args):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(X[:, 0], X[:, 1], y, label='Observed')
-    ax.plot_wireframe(ax_1, ax_2, y_, color='indianred', label='Prediction')
-    plt.show()
+    ax.plot_wireframe(ax_1, ax_2, y_, color='indianred', label=reg_eq_3)
+    plt.legend()
+    plt.savefig(result_path/'data3.png')
 
 
 if __name__ == "__main__":
