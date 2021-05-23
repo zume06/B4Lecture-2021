9813166d3d4e076ce3b42f0293546a7b752e4fd0
梶原大進 <daishin@kajiwarataishinnoMacBook-puro.local>
2021-05-23 20:59:32 +0900

    cepstrum method

--- ex_4/kajiwara/main.py
+++ ex_4/kajiwara/main.py
@@ -8,7 +8,7 @@ import librosa.display
 import matplotlib.pyplot as plt
 
 from modules.autocorrelation import autocorrelation, get_ac_peaks
-from modules.cepstrum import get_cepstrum, get_ceps_peaks
+from modules.cepstrum import get_cepstrum, get_ceps_peaks, get_envelope
 from modules.spectrogram import spectrogram
 
 TIME_TEMPLATE = '%Y%m%d%H%M%S'
@@ -47,7 +47,7 @@ def main(args):
     plt.clf()
     plt.close()
 
-    ceps_db = get_cepstrum(wave_data)
+    ceps_db = get_cepstrum(wave_data, is_clipping=False, is_framing=True)
     peak_index = get_ceps_peaks(ceps_db, sr)
     f0 = sr / peak_index
     times = np.linspace(0, (wave_data.size - win_size) / sr, f0.size)
@@ -59,6 +59,22 @@ def main(args):
     plt.xlabel("Time[sec]")
     plt.ylabel("Frequency[Hz]")
     plt.savefig(result_path/'f0_ceps.png')
+    plt.clf()
+    plt.close()
+
+    clip_size = 8192
+    ceps_db = get_cepstrum(wave_data, is_clipping=True, is_framing=False, clip_size=clip_size)
+    ceps_env = get_envelope(ceps_db, 100)
+    freq = np.fft.rfft(wave_data, clip_size)
+    amp = np.log(np.abs(freq))
+    fscale = np.fft.fftfreq(clip_size, d=1.0 / sr)
+    plt.plot(fscale[:clip_size//2], amp[:clip_size//2], label='spectrum')
+    plt.plot(fscale[:clip_size//2], ceps_env[:clip_size//2], label='cepstrum')
+    plt.title("amplitude characteristic")
+    plt.xlabel("Frequency[Hz]")
+    plt.ylabel("Amplitude")
+    plt.legend()
+    plt.savefig(result_path/'enveloop.png')
     plt.clf()
     plt.close()
 
