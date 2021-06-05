a4f8161a6c585fd0057dfde46999935e8b587331
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2021-06-05 11:21:03 +0900

    add contribution_rate as method

--- ex_6/a_miyashita/main.py
+++ ex_6/a_miyashita/main.py
@@ -6,18 +6,27 @@ from numpy.linalg.linalg import norm
 
 
 def normalize(data, axis=None):
+    """
+        Normalize input data.
+
+        # Args
+            data (ndarray): input data
+            axis ({int, None}, default=None):
+                Axis that statistics are calculated along. If axis==None, 
+                data is normalized along all dimensions.
+
+        # Returns
+            out (ndarray): normalized data
+    """
     mean = np.mean(data, axis=axis, keepdims=True)
     std = np.std(data, axis=None, keepdims=True)
     return (data - mean) / std
 
 
 class Pca:
-    def __init__(self):
-        """
-            Class for principal component analysis.
-        """
-        return
-
+    """
+        Class for principal component analysis.
+    """
     def fit(self, x, y=None):
         """
             Calculate principal component of given data.
@@ -54,6 +63,22 @@ class Pca:
         """
         return x @ self.W
 
+    def contribution_rate(self):
+        """
+            Calculate contribution rate.
+
+            # Returns
+                cr (ndarray, axis=(dimension,)): contribution rate
+                ccr (ndarray, axis=(dimension,)): cumlative contribution rate
+        """
+        # calculate contribution rate
+        cr = self.lam / np.sum(self.lam)
+        # calculate cumlative contribution rate
+        ccr = np.cumsum(cr)
+        ccr = np.insert(ccr, 0, 0)
+        return cr, ccr
+
+
 
 def main():
     # process args
@@ -61,14 +86,17 @@ def main():
     parser.add_argument("sc", type=str, help="input filename followed by extention .csv")
     args = parser.parse_args()
 
-    data = np.loadtxt(f"{args.sc}.csv", delimiter=',', skiprows=1)
+    data = np.loadtxt(f"{args.sc}.csv", delimiter=',')
+    print(f">>> data = np.loadtxt({args.sc}.csv, delimiter=',')")
+    print(">>> print(data.shape)")
+    print(data.shape)
 
     data = normalize(data, axis=0)
 
     pca = Pca()
     pca.fit(data)
     # calculate contribution rate
-    cr = pca.lam / np.sum(pca.lam)
+    cr, ccr = pca.contribution_rate()
 
     if data.shape[1] == 2:
         # plot
@@ -108,12 +136,9 @@ def main():
         plt.show()
 
     if data.shape[1] >= 4:
-        # calculate cumlative contribution rate
-        ccr = np.cumsum(cr)
-        ccr = np.insert(ccr, 0, 0)
-
         # plot
         plt.plot(ccr)
+        plt.plot([0, ccr.size], [0.9, 0.9], linestyle='dashed')
         plt.title(f"{args.sc} CCR")
         plt.xlabel("dimension")
         plt.ylabel("cumlative contribution rate")
@@ -125,6 +150,14 @@ def main():
         plt.title(args.sc)
         plt.xlabel("$z_0$")
         plt.ylabel("$z_1$")
+        plt.legend(bbox_to_anchor=(1, 0), loc="lower right", borderaxespad=1)
+        plt.axes().set_aspect('equal')
+        plt.show()
+
+        plt.scatter(data_pca[:,98], data_pca[:,99], facecolor='None', edgecolors='r')
+        plt.title(args.sc)
+        plt.xlabel("$z_{{98}}$")
+        plt.ylabel("$z_{{99}}$")
         plt.legend(bbox_to_anchor=(1, 0), loc="lower right", borderaxespad=1)
         plt.axes().set_aspect('equal')
         plt.show()
